f183ecb3ad3bdb04d425d27136e6bfca187eddb4
Akira Kawai <38210940+akiralab@users.noreply.github.com>
2022-11-01 13:05:31 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View SafeAreaView>
+      <Text>test</Text>
+    </View>
+  );
+}
